d3d3a10935d102168567027a7deb57abe398a84e
Diego Blanco Guerrero <diego@Ordenador-portatil-de-Diego.local>
2026-04-11 03:59:35 +0200

    añadir páginas al sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,18 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.monumentosllenos.com',
+      url: "https://monumentosllenos.com",
       lastModified: new Date(),
     },
-  ]
+    {
+      url: "https://monumentosllenos.com/eventos",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://monumentosllenos.com/participa",
+      lastModified: new Date(),
+    },
+  ];
 }
